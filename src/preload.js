4e5c99942ca4d3a08856dad5bb83f1b04c972df2
Ryan Maxie <MichaelRMaxie@Gmail.com>
2025-02-12 10:42:52 -0600

    Refactor main.js, preload.js, and renderer.js to use arrow functions for consistency

--- src/preload.js
+++ src/preload.js
@@ -3,8 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   sendMessage: (message) => ipcRenderer.send('send-message', message),
   onReceiveMessage: (callback) => {
-    console.log('onReceiveMessage Hit!'); // Debugging
-    ipcRenderer.removeAllListeners('receive-message');
+        ipcRenderer.removeAllListeners('receive-message');
     ipcRenderer.on('receive-message', (event, message) => callback(message));
   },
 });
